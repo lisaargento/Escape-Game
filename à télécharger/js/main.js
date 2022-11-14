ac2cc1b65c4052dbf89c5af69fa4dd95f33c236c
GirardinClaire <116156608+GirardinClaire@users.noreply.github.com>
2022-11-14 23:27:47 +0100

    Update main.js

--- à télécharger/js/main.js
+++ à télécharger/js/main.js
@@ -15,7 +15,7 @@ map.setView(positionDepart, 15);
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 22,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
