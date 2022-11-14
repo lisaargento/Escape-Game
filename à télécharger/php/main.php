f27b070f07cc9246bfd256da6d17e0128a1346b7
GirardinClaire <116156608+GirardinClaire@users.noreply.github.com>
2022-11-14 23:59:20 +0100

    Add files via upload

---
+++ à télécharger/php/main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/accueil.css"/>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"/>
+    <title>Escape Game </title>
+  </head>
+
+  <body>
+    <div id="bouttons">
+      <a id="commencer" href="../php/inscription.php">Commencer !</a>
+      <a id="resultats" href="../resultats.html">Résultats</a>
+    </div>
+
+    <div id="map"></div>
+
+    <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js"></script>
+    <script src='../js/main.js'></script>
+
+  </body>
+
+</html>
+
+
+
+
