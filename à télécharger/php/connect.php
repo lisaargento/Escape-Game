100740ea81331dc9ab3d89bc6da5f85a705038af
GirardinClaire <116156608+GirardinClaire@users.noreply.github.com>
2022-11-14 22:39:09 +0100

    Add files via upload

---
+++ à télécharger/php/connect.php
@@ -0,0 +1,15 @@
+<?php
+
+$link = mysqli_connect('localhost', 'root', 'root', 'BdD');
+
+if (!$link) {
+  die('Erreur de connexion');
+} else {
+  echo 'Succès... ';
+}
+
+// Pour éviter les problèmes d'encodage entre PHP et la base de données
+mysqli_set_charset($link, "utf8");
+
+?>
+
