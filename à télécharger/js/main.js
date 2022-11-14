871c85c4f8baa127981eb52545e139faafc1a4be
GirardinClaire <116156608+GirardinClaire@users.noreply.github.com>
2022-11-14 22:36:31 +0100

    Update main.js

--- à télécharger/js/main.js
+++ à télécharger/js/main.js
@@ -1,8 +1,23 @@
-
+/*
 let today = new Date();
 today.getTime(); // le timestamp actuel
 
 Date.now() // le timestamp actuel (méthode statique)
 console.log(Date);
+*/
+
+var map = L.map('map');
+
+// Position de départ du jeu
+let positionDepart = [48.8410, 2.5875];
+map.setView(positionDepart, 10);
+
+var marker = L.marker([latitude0, longitude0]).addTo(map);
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 
