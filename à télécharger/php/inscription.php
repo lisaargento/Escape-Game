5cfb5f6d9b7304b760048550bf845037eb6cf20b
GirardinClaire <116156608+GirardinClaire@users.noreply.github.com>
2022-11-15 01:55:42 +0100

    Update inscription.php

--- à télécharger/php/inscription.php
+++ à télécharger/php/inscription.php
@@ -10,28 +10,30 @@
     <?php 
           include("../php/connect.php"); 
     ?>
+
     <h1 id="titre">Un tour de France</h1>
+
     <div>
       <a id="sortir" href="../accueil.html"><img src="../img/sortir1.png" style="width:90px;height:70px;"></a>
     </div>
-        <form action="#" method="get" id="inscription">
-            <fieldset>
-            <legend>Inscription</legend>
-            <p><label>Nom <input type="text" name="nom" placeholder="Duck"></label></p>
-            <p><label>Prénom <input type="text" name="prenom" placeholder="Daffy"></label></p>
-            <p><label>Pseudo <input type="text" name="pseudo" placeholder="CoIGN-CoIGN l'explorateur"></label></p>
-            <p><input type="submit" name="valider" value="Valider" id="valider"></p>
-            </fieldset>
-        </form>
 
-        <h1 id="slogan">To escape from Champs-sur-Marne</h1>
-
-        <script src="../js/inscription.js" charset="utf-8"></script>
-        
+    <form action="#" method="get" id="inscription">
+      <fieldset>
+      <legend>Inscription</legend>
+        <p><label>Nom <input type="text" name="nom" placeholder="Duck"></label></p>
+        <p><label>Prénom <input type="text" name="prenom" placeholder="Daffy"></label></p>
+        <p><label>Pseudo <input type="text" name="pseudo" placeholder="CoIGN-CoIGN l'explorateur"></label></p>
+        <p><input type="submit" name="valider" value="Valider" id="valider"></p>
+      </fieldset>
+    </form>
     
+    <h1 id="slogan">To escape from Champs-sur-Marne</h1>
+
+    <script src="../js/inscription.js" charset="utf-8"></script>
+        
   </body>
   
-  </html>
+</html>
 
 
 
