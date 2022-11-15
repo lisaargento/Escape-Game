3869be8299d503b8c5d09b797bec3cf7716e455a
GirardinClaire <116156608+GirardinClaire@users.noreply.github.com>
2022-11-15 01:34:51 +0100

    Add files via upload

---
+++ à télécharger/php/resultats.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <title>Escape Game </title>
+    <meta> 
+    <link rel="stylesheet" href="../css/accueil.css"/>
+  </head>
+
+  <body id="menu">
+    <h1 id="titre">Un tour de France</h1>
+
+    <a id="commencer" href="../accueil.html">Commencer !</a>
+
+    
+  </body>
+  
+  </html>
