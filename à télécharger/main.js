28886d19f200bc5ecd917f44659e9742c7863c6f
GirardinClaire <116156608+GirardinClaire@users.noreply.github.com>
2022-11-15 19:09:27 +0100

    Add files via upload

---
+++ à télécharger/main.js
@@ -0,0 +1,22 @@
+/*
+let today = new Date();
+today.getTime(); // le timestamp actuel
+
+Date.now() // le timestamp actuel (méthode statique)
+console.log(Date);
+*/
+
+var map = L.map('map');
+
+// Position de départ du jeu
+let positionDepart = [48.8410, 2.5875];
+map.setView(positionDepart, 15);
+
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
