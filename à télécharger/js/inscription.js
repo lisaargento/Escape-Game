0e8868d06b2a1178d9dd7e839fbc9ed0d4d02f7b
GirardinClaire <116156608+GirardinClaire@users.noreply.github.com>
2022-11-15 01:55:13 +0100

    Update inscription.js

--- à télécharger/js/inscription.js
+++ à télécharger/js/inscription.js
@@ -1,39 +1,54 @@
+// Récupérer l'élément form par id = "inscription"
 let form_inscription = document.getElementById("inscription");
 
+// Récupérer des champs du formulaire par leurs name
 let champ_nom = form_inscription.elements["nom"];
 let champ_prenom = form_inscription.elements["prenom"];
 let champ_pseudo = form_inscription.elements["pseudo"];
 
+// Ajouter l'évènement submit correspondant à la validation du formulaire
+form_inscription.addEventListener('submit', validationformulaire);
 
-
-function valider (event) {  // code a exécuter lorsque le formulaire sera validé
+// Fonction de validation du formulaire : tester la conformité des champs
+function validationformulaire (event) {
     // le formulaire est-il OK?
     let form_OK = true;
 
-    //vérification présence nom
-    if(champ_prenom==""){
+    // Champ nom vide ?
+    if(champ_nom.value == ''){
         form_OK = false;
         champ_nom.classList.add("erreur");
-        console.log("oui");
-    }else{champ_nom.classList.remove("erreur");}
+    }
+    else{
+        champ_nom.classList.remove("erreur");
+    }
 
-    //vérification présence prénom
-    if(champ_nom==""){
+    // Champ prenom vide ?
+    if(champ_prenom.value == ''){
         form_OK = false;
-        champ_nom.classList.add("erreur");
-    }else{champ_nom.classList.remove("erreur");}
+        champ_prenom.classList.add("erreur");
+    }
+    else{
+        champ_prenom.classList.remove("erreur");
+    }
 
-    //vérification présence prénom
-    if(champ_pseudo==""){
+    // Champ pseudo vide ?
+    if(champ_pseudo.value == ''){
         form_OK = false;
         champ_pseudo.classList.add("erreur");
-    }else{champ_nom.classList.remove("erreur");}
+    }
+    else{
+        champ_pseudo.classList.remove("erreur");
+    }
 
-    // Au final, on empeche l'envoi du formulaire si form_OK est faux
+    // Si form_OK est faux, on empeche l'envoi du formulaire
     if(!form_OK){
         event.preventDefault();
-    }  
+        alert("Au moins l'un des 3 champs n'est pas bien renseigné ...")
+    }
+
 }
-//ajoute l’événement
-form_inscription.addEventListener('submit', valider);
+
+
+
 
