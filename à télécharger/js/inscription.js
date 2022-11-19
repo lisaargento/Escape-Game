d0a9eba9bd5614716ce576bbe7d9a958b72b6c3f
GirardinClaire <116156608+GirardinClaire@users.noreply.github.com>
2022-11-20 00:50:46 +0100

    Add files via upload

--- à télécharger/js/inscription.js
+++ à télécharger/js/inscription.js
@@ -2,9 +2,10 @@
 let form_inscription = document.getElementById("inscription");
 
 // Récupérer des champs du formulaire par leurs name
-let champ_nom = form_inscription.elements["nom"];
-let champ_prenom = form_inscription.elements["prenom"];
-let champ_pseudo = form_inscription.elements["pseudo"];
+let nom = form_inscription.elements["nom"];
+let prenom = form_inscription.elements["prenom"];
+let pseudo = form_inscription.elements["pseudo"];
+
 
 // Ajouter l'évènement submit correspondant à la validation du formulaire
 form_inscription.addEventListener('submit', validationformulaire);
@@ -15,40 +16,40 @@ function validationformulaire (event) {
     let form_OK = true;
 
     // Champ nom vide ?
-    if(champ_nom.value == ''){
+    if(nom.value == ''){
         form_OK = false;
-        champ_nom.classList.add("erreur");
+        nom.classList.add("erreur");
     }
     else{
-        champ_nom.classList.remove("erreur");
+        nom.classList.remove("erreur");
     }
 
     // Champ prenom vide ?
-    if(champ_prenom.value == ''){
+    if(prenom.value == ''){
         form_OK = false;
-        champ_prenom.classList.add("erreur");
+        prenom.classList.add("erreur");
     }
     else{
-        champ_prenom.classList.remove("erreur");
+        prenom.classList.remove("erreur");
     }
 
     // Champ pseudo vide ?
-    if(champ_pseudo.value == ''){
+    if(pseudo.value == ''){
         form_OK = false;
-        champ_pseudo.classList.add("erreur");
+        pseudo.classList.add("erreur");
     }
     else{
-        champ_pseudo.classList.remove("erreur");
+        pseudo.classList.remove("erreur");
     }
 
     // Si form_OK est faux, on empeche l'envoi du formulaire
-    if(!form_OK){
+    if (!form_OK){
         event.preventDefault();
-        alert("Au moins l'un des 3 champs n'est pas renseigné ...")
+        alert("Au moins l'un des 3 champs n'est pas renseigné ...");
     }
+    
+    fetch('../php/inscription.php?nom='+nom.value+'&prenom='+prenom.value+'&pseudo='+pseudo.value);
 
 }
-
-
 
 
--- à télécharger/js/main.js
+++ à télécharger/js/main.js
@@ -1,3 +1,5 @@
+//  !!!!! A FAIRE !!!! Gestion du temps de la partie
+
 /*
 let today = new Date();
 today.getTime(); // le timestamp actuel
@@ -6,17 +8,70 @@ Date.now() // le timestamp actuel (méthode statique)
 console.log(Date);
 */
 
+
+// AFFICHAGE DE LA CARTE
 var map = L.map('map');
 
 // Position de départ du jeu
-let positionDepart = [48.8410, 2.5875];
+let positionDepart = [48.841470, 2.587863];
 map.setView(positionDepart, 15);
-
-
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+
+
+// AFFICHAGE DES OBJETS
+
+var img = L.icon({
+    iconUrl: '../img/VelosCasses.png',
+    iconSize:     [64, 64], // taille de l'icone
+    iconAnchor:   [32, 64], // point de l'icone qui correspondra à la position du marker
+    popupAnchor:  [-10, -76] // point depuis lequel la popup doit s'ouvrir relativement à l'iconAnchor
+});
+
+var marker = L.marker([48.841470, 2.587863], {icon: img});
+
+
+// Apparition selon le zoom
+
+map.on('zoomend', function(){
+    if (map.getZoom()<18){
+        marker.remove();
+    }
+    else {
+        marker.addTo(map);
+    }
+})
+
+
+
+// GERER LES OBJETS
+
+// à voir ...
+
+/*
+
+
+let formBottom = document.getElementById('formBottom')
+formBottom.addEventListener('submit', ville);
+
+function ville (event) {
+  event.preventDefault();
+  let select = documet.getElementById('select_communes');
+
+  let val = select.value;
+  fetch('requetemain.php?id='+valeurID)
+  .then(result => result.json())
+  .then(json => {
+    let data = JSON.parse(json.geom);
+    L.geoJSON(data).addTo(map);
+  })
+}
+
+*/
 
 
