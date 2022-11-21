e0842f4b72ca11f4f7afe59bc547e453d28afebd
Claire_GRD <claire-girardin@hotmail.fr>
2022-11-21 21:51:01 +0100

    2151

--- à télécharger/php/inscription.php
+++ à télécharger/php/inscription.php
@@ -1,6 +1,18 @@
 <?php 
 
-include('connect.php');
+// CONNEXION A LA BDD
+$link = mysqli_connect('localhost', 'root', 'root', 'bddEscapeGame');
+
+if (!$link) {
+  die('Erreur de connexion');
+}
+
+// Pour éviter les problèmes d'encodage entre PHP et la base de données
+mysqli_set_charset($link, "utf8");
+
+
+
+// INSERTION DES DONNEES
 
 if( isset( $_GET['nom'] ) ) {
     $nom = $_GET['nom'];
