5b87deb7776ba3105c8b0c963cf5f243b5e79104
lisa.argento <lisa.argento@ensg.eu>
2022-11-23 22:26:46 +0100

    modif bdd

--- à télécharger/jeu.php
+++ à télécharger/jeu.php
@@ -17,12 +17,20 @@ include('php/inscription.php');
   </head>
 
   <body>
-
+    <div id="barre">
+      <p>Un tour de France</p>
+      <p> le chrono</p>
+      <p><a id="sortir" href="accueil.html"><img src="img/sortir1.png" style="width:90px;height:70px;"></a></p>
+    </div>
+<!--
     <div>
       <a id="sortir" href="accueil.html"><img src="img/sortir1.png" style="width:90px;height:70px;"></a>
     </div>
-
-    <div id="map"></div>
+-->
+    <div id="jeu">
+      <div id="map"></div>
+      <div id="inventaire"></div>
+    </div>
 
     <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js"></script>
     
