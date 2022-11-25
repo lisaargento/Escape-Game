64abcdbae93f5bed811a0b3f1f78b8f06eba717c
lisa.argento <lisa.argento@ensg.eu>
2022-11-25 15:44:39 +0100

    bdd à jour

--- à télécharger/bddescapegame.sql
+++ à télécharger/bddescapegame.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 24, 2022 at 02:47 PM
+-- Generation Time: Nov 25, 2022 at 02:38 PM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -44,16 +44,16 @@ CREATE TABLE `jeu` (
 --
 
 INSERT INTO `jeu` (`id`, `nomobjet`, `indice`, `latitute`, `longitude`, `minzoom`, `icone`, `type`, `idbloque`) VALUES
-(1, 'velos_casses', 'Ohhh non ! Nos vélos sont cassés ...\r\nMais il me semble que les ENSGagés ont mis à disposition des outils qui pourraient nous servir ;) ', 48.8415, 2.58786, 18, '../img/velos_casses.png', 4, 0),
+(1, 'velos_casses', 'Ohhh non ! Nos vélos sont cassés ...\r\nMais il me semble que les ENSGagés ont mis à disposition des outils qui pourraient nous servir ;) ', 48.8415, 2.58786, 18, '../img/velos_casses.png', 4, 5),
 (2, 'cadenas', 'Il me semble que code est la date de la création de l’Académie du Climat de Paris, Place Baudoyer, tu sais à côté de l’hôtel de ville de Paris?', 48.841, 2.58736, 20, '../img/cadenas.png', 3, 4),
-(3, 'plaque', '', 48.8563, 2.35584, 17, '../img/plaque.jpg', 1, 0),
-(4, 'boite à outils', '', 48.841, 2.58736, 15, '../img/outils.png', 2, 5),
-(5, 'velos_repares', 'appel entrant de Lisa ', 48.8415, 2.58786, 18, '../img/velos_repares.png', 2, 0),
-(6, 'place', 'J’avais prévu qu’on aille voir la plus grande fierté des Montois avant de commencer le tour de Bretagne.)', 48.2766, -3.55418, 16, '../img/place.png', 3, 0),
+(3, 'plaque', '', 48.8563, 2.35584, 17, '../img/plaque.jpg', 1, 2),
+(4, 'boite à outils', '', 48.841, 2.58736, 15, '../img/outils.png', 2, 1),
+(5, 'velos_repares', 'appel entrant de Lisa ', 48.8415, 2.58786, 18, '../img/velos_repares.png', 2, 6),
+(6, 'place', 'J’avais prévu qu’on aille voir la plus grande fierté des Montois avant de commencer le tour de Bretagne. \r\n(place bloqué par deux objets, les deux photos)', 48.2766, -3.55418, 16, '../img/place.png', 3, 0),
 (7, 'photo_mont', 'Tout est dans la photo!', 48.6358, -1.51194, 16, '../img/photo_mont.jpg', 1, 6),
 (8, 'photo_dinan', '', 48.4572, -2.03774, 16, '../img/photo_dinan.jpg', 1, 6),
 (9, 'bon', '', 48.2139, -4.53925, 12, '../img/bon.png', 2, 10),
-(10, 'crepe', 'Elle manque de beurre ta crêpe. On m’a dit que la Biscuiterie de Carnac avait les meilleurs mottes de beurre salé.', 47.8555, -3.90607, 17, 'crepe.png', 4, 0),
+(10, 'crepe', 'Elle manque de beurre ta crêpe. On m’a dit que la Biscuiterie de Carnac avait les meilleurs mottes de beurre salé.\r\n(bloquée par bon et beurre)', 47.8555, -3.90607, 17, 'crepe.png', 4, 0),
 (11, 'beurre', '', 47.5735, -3.07483, 18, '../img/beurre.png', 2, 10);
 
 -- --------------------------------------------------------
@@ -82,7 +82,10 @@ INSERT INTO `joueurs` (`id`, `nom`, `prenom`, `pseudo`, `temps`, `etape`, `score
 (5, 'Girardin', 'Claire', 'Clairette', NULL, NULL, NULL, NULL),
 (6, 'Argento', 'Lisa', 'lisaaaaa', NULL, NULL, NULL, NULL),
 (7, 'ARGENTO', 'Jean-Philippe', 'JP', NULL, NULL, NULL, NULL),
-
+(8, 'Argento', 'Lisa', 'efr', NULL, NULL, NULL, NULL),
+(9, 'Argento', 'Lisa', 'efr', NULL, NULL, NULL, NULL),
+(10, 'Argento', 'Lisa', 'efr', NULL, NULL, NULL, NULL),
+(11, 'Argento', 'Lisa', 'efr', NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
