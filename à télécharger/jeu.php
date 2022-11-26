e0f8f8d86ff6e7c9b0a2b4884101dc075f985fe5
lisa.argento <lisa.argento@ensg.eu>
2022-11-26 18:13:55 +0100

    modif logo + code mais inventaire pb

--- à télécharger/jeu.php
+++ à télécharger/jeu.php
@@ -13,27 +13,22 @@ include('php/inscription.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/jeu.css"/>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"/>
-    <title>Escape Game </title>
+    <link rel="icon" href="img/logo.ico" />
+    <title>Un tour de France</title>
   </head>
 
   <body>
     <div id="barre">
       <p>Un tour de France</p>
-      <p> le chrono</p>
-      <p><a id="sortir" href="accueil.html"><img src="img/sortir1.png" style="width:90px;height:70px;"></a></p>
+      <p>le chrono</p>
+      <p><a id="sortir" href="accueil.html"><img src="img/sortir.png" style="width:90px;height:70px;"></a></p>
     </div>
-<!--
-    <div>
-      <a id="sortir" href="accueil.html"><img src="img/sortir1.png" style="width:90px;height:70px;"></a>
-    </div>
--->
     <div id="jeu">
       <div id="map"></div>
       <div id="inventaire"></div>
     </div>
 
     <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js"></script>
-    
     <script src='js/main.js'></script>
 
   </body>
