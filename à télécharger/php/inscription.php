ea8183f972dba7c9474763da4ac75920d879e9c3
lisa.argento <lisa.argento@ensg.eu>
2022-12-04 00:16:21 +0100

    00:16

--- à télécharger/php/inscription.php
+++ à télécharger/php/inscription.php
@@ -20,7 +20,7 @@ if( isset( $_GET['nom'] ) ) {
     $nom = $_GET['nom'];
     $prenom = $_GET['prenom'];
     $pseudo = $_GET['pseudo'];
-    $request = " INSERT INTO joueurs (nom, prenom, pseudo) VALUES ('$nom', '$prenom', '$pseudo');";
+    $request = " INSERT INTO joueurs (nom, prenom, pseudo) VALUES ('$nom', '$prenom', '$pseudo') ";
     mysqli_query($link, $request);
 }
 
--- à télécharger/php/resultats_perso.php
+++ à télécharger/php/resultats_perso.php
@@ -8,10 +8,10 @@ include('connect.php');
 if( isset( $_GET['temps'] ) ) {
     $temps = $_GET['temps'];
     $score = $_GET['score'];
-    $request = "UPDATE joueurs SET temps='$temps', score='$score' WHERE temps IS NULL";
+    $request = " UPDATE joueurs SET temps=$temps, score=$score WHERE temps == NULL";
     mysqli_query($link, $request);
 }
 
 
-?>
 
+?>
