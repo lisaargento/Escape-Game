0ba1cd3ad23f35d3d50323e9319bb7e7e845d421
lisa.argento <lisa.argento@ensg.eu>
2022-12-03 12:47:24 +0100

    12:46 bddescapegame à jour + speech début modified

--- à télécharger/bddescapegame.sql
+++ à télécharger/bddescapegame.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 03, 2022 at 11:22 AM
+-- Generation Time: Dec 03, 2022 at 11:46 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -49,11 +49,11 @@ CREATE TABLE `jeu` (
 --
 
 INSERT INTO `jeu` (`id`, `nomobjet`, `indice`, `latitude`, `longitude`, `width`, `height`, `minzoom`, `URLicone`, `type`, `idSolution`, `idDebloquant`, `idLibere`, `audio`) VALUES
-(1, 'Vélos cassés', 'Ohhh non ! Mon vélo a crevé ...\r\n<br>\r\nIl me semble que les ENSGagés ont mis à disposition des outils qui pourraient nous servir ;) ', 48.8415, 2.58786, 100, 100, 18, '../img/velos_casses.png', 4, 2, 4, 5, ''),
+(1, 'Vélos cassés', 'Ohhh non ! Mon vélo a crevé ...\r\n<br>\r\nIl me semble que les ENSGagés ont mis à disposition des outils qui pourraient nous servir ;) ', 48.8561, 2.614, 100, 100, 18, '../img/velos_casses.png', 4, 2, 4, 5, ''),
 (2, 'Cadenas', 'Mince la porte de la salle est fermée ...\r\n<br>\r\nIl me semble que le code de ce cadenas est la date de la création de l’Académie du Climat de Paris ! Elle se situe Place Baudoyer à côté de l’hôtel de ville de Paris, ça te dis quelque chose ?', 48.841, 2.58736, 50, 50, 19, '../img/cadenas.png', 3, 3, 2021, 4, ''),
 (3, 'Plaque', 'Le saviez-vous ?\r\n<br> <br>\r\nL\'Académie du Climat est un lieu pour s\'informer, se former, échanger et agir !\r\n<br>\r\nElle regroupe les locaux de plusieurs associations engagées et accueille des ateliers, des conférences/débats, des projections, des expositions... Mais aussi des classes et des groupes pour les accompagner de la sensibilisation à l\'engagement.', 48.8563, 2.35584, 250, 200, 17, '../img/plaque.jpg', 1, NULL, NULL, NULL, ''),
 (4, 'Boite à outils', 'Le saviez-vous ?\r\n<br><br>\r\nRéparer et revaloriser nos biens est très important car la production et l\'incinérations sont les étapes les plus polluantes dans le cycle de vie des objets.\r\n<br>\r\nEn donnant une seconde vie à nos biens nous évitons d\'en produire de nouveaux et nous réduisons nos déchets. ', 48.841, 2.58736, 100, 70, 18, '../img/outils.png', 2, NULL, NULL, NULL, ''),
-(5, 'Vélos réparés', '', 48.8415, 2.58786, 100, 100, 18, '../img/velos_repares.png', 2, NULL, NULL, 6, '../audio/depart.m4a'),
+(5, 'Vélos réparés', '', 48.8561, 2.614, 100, 100, 18, '../img/velos_repares.png', 2, NULL, NULL, 6, '../audio/depart.m4a'),
 (6, 'Places', 'J’avais prévu qu’on aille voir la plus grande fierté des Montois avant de commencer le tour de Bretagne.', 48.2766, -3.55418, 100, 100, 16, '../img/place.png', 3, 7, 5712, 9, ''),
 (7, 'Mont Saint Michel', 'Le saviez-vous ?\r\n<br><br>\r\nCet îlot rocheux consacré à saint Michel est une étape des Chemins de Saint-Jacques-de-Compostelle. Il figure, avec sa baie, sur la liste du patrimoine mondiale de l\'UNESCO depuis 1979. A l\'origine nommé Mont Tombe, il y fut érigé un oratoire en l\'honneur de l\'archange saint Michel en 708 qui lui donnera plus tard l\'appellation qu\'on lui connait aujourd\'hui.', 48.6358, -1.51194, 500, 400, 17, '../img/photo_mont.jpg', 1, 8, NULL, NULL, ''),
 (8, 'Dinan', '', 48.4572, -2.03774, 250, 200, 15, '../img/photo_dinan.jpg', 1, NULL, NULL, NULL, ''),
@@ -92,7 +92,8 @@ INSERT INTO `joueurs` (`id`, `nom`, `prenom`, `pseudo`, `temps`, `etape`, `score
 (9, 'Argento', 'Lisa', 'efr', NULL, NULL, NULL, NULL),
 (21, 'Gir', 'Claire', 'clairette', NULL, NULL, NULL, NULL),
 (22, 'c c', 'vreg', 'ss', NULL, NULL, NULL, NULL),
-(23, 'ARGENTO', 'Lisa', 'CoIGN-CoIGN', NULL, NULL, NULL, NULL);
+(23, 'ARGENTO', 'Lisa', 'CoIGN-CoIGN', NULL, NULL, NULL, NULL),
+(24, 'ARGENTO', 'Lisa', 'CoIGN-CoIGN', NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -124,7 +125,7 @@ ALTER TABLE `jeu`
 -- AUTO_INCREMENT for table `joueurs`
 --
 ALTER TABLE `joueurs`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
