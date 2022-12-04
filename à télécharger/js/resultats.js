f4ce24673f91c5028c08940b834823061b031886
Claire_GRD <claire-girardin@hotmail.fr>
2022-12-05 00:54:19 +0100

    fin Claire

--- à télécharger/js/resultats.js
+++ à télécharger/js/resultats.js
@@ -9,12 +9,13 @@ fetch('../php/resultats.php')
 
 // Affichage du HallOfFame
 function HallOfFame(infoJson) {
-    for (let i = 0; i < 5; i++) {
-        var pseudo = infoJson[i]['pseudo'];
-        var score = infoJson[i]['score'];
-        var temps = infoJson[i]['temps'];
-        console.log(pseudo);
-        var tableau = document.getElementById('p'+i);
+    var listHallOfFame = ['p1', 'p2', 'p3', 'p4', 'p5'];
+    for (let i = 1; i <= 5; i++) {
+        var pseudo = infoJson[i-1]['pseudo'];
+        var score = infoJson[i-1]['score'];
+        var temps = infoJson[i-1]['temps'];
+        var id = listHallOfFame[i-1];
+        var tableau = document.getElementById(id);
         tableau.innerHTML = '<td>'+i+'</td><td>'+pseudo+'</td><td>'+score+'</td><td>'+temps+'</td>';
     }
 
